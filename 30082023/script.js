d99f003f270f8d7610699d96113c2d0a2dcf3e0f
Paulo Renan Barroso Guerra <144187075+prwith2@users.noreply.github.com>
2023-09-06 11:47:24 -0300

    Add files via upload

--- 30082023/script.js
+++ 30082023/script.js
@@ -85,29 +85,50 @@ function login(){
    console.log("Esse console log mostra o array completo")
 }
 
-function objeto(){
-   //Produtos
-   let carrinho_de_compras = []
-   // let nome = "Arroz"
-   // let preco = 9.99
-   // let marca = "Tio João"
-
-   let produto = {
-      nome_prod: "Arroz", 
-      preco_prod: 9.99, 
-      marca_prod: "Tio João"
+function array(){
+   let n = [3, 4, 5, 6, 2, 1,]
+   console.log("Esse console log mostra o array completo: " + n)
+   console.log(n)
+   console.log("Esse console log mostra o indice zero: " + n[0])
+   n[6]=7
+   console.log("Esse e o indice 6: " + n[6])
+   n.push(68)
+   n.push(70)
+   // push = empurrar
+   console.log("Novo array completo: ")
+   console.log(n)
+   n.pop()
+   console.log("Novo Log depois da atualização")
+   console.log(n)
+   //tira o ultimo  elemento do array
+   
+   
    }
-   
-   carrinho_de_compras.push(produto);
-   
-   let produto2 = {
-      nome_prod: "Feijão", 
-      preco_prod: 7.35, 
-      marca_prod: "Namorado"
+   function produto(){
+       let product = document.getElementById("frmProduto").value
+       let li = document.createElement("li") 
+       li.innerHTML=product
+       document.getElementById("li1").appendChild(li)
+       console.log(product)
    }
-   
-   carrinho_de_compras.push(produto2);
-
-   console.log(carrinho_de_compras)
-   console.log(carrinho_de_compras[0].nome_prod)
-}
+   function valor(){
+       let Valor = document.getElementById("frmValor").value
+       let li = document.createElement("li") 
+       li.innerHTML=Valor
+       document.getElementById("li2").appendChild(li)
+       console.log(Valor)
+   }
+   function marca(){
+       let marca = document.getElementById("frmMarca").value
+       let li = document.createElement("li") 
+       li.innerHTML=marca
+       document.getElementById("li3").appendChild(li)
+       console.log(marca)
+   }
+   document.addEventListener("DOMContentLoaded", function() {
+       document.getElementById("botao").addEventListener("click", function(){
+           produto();
+           valor();
+           marca();
+       });
+   });
